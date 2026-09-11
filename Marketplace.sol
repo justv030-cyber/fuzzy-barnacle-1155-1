79216b87c55fb52c45a1cef56194590e43a52665
justv030-cyber <justv030@gmail.com>
2026-09-11 19:07:50 +0530

    udpate in buy

--- Marketplace.sol
+++ Marketplace.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.34;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/IERC1155.sol";
+import "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/ReentrancyGuard.sol";
 
-contract marketPlace {
+contract marketPlace is ReentrancyGuard  {
     uint256 public listingId;
     IERC1155 public NFT;
+
+    uint256 marketplaceFee=250;//2.5%
 
     struct Listing {
         address sellerAddress;
@@ -45,12 +48,12 @@ contract marketPlace {
         });
     }
 
-    function butItem(uint256 _listingId) public payable {
+    function buyItem(uint256 _listingId) public payable nonReentrant {
         require(listings[_listingId].active == true, "Listing Is Not Active");
 
         uint256 price = listings[_listingId].pricePerItem *
             listings[_listingId].amount;
-        require(msg.value >= price, "Insufficient Funds");
+        require(msg.value == price, "Insufficient Funds");
 
         NFT.safeTransferFrom(
             listings[_listingId].sellerAddress,
@@ -60,6 +63,17 @@ contract marketPlace {
             ""
         );
 
+
+
         listings[_listingId].active = false;
+
+        uint256 Fees = msg.value * marketplaceFee / 10000;
+
+        uint256 sendPrice = msg.value - Fees;
+
+        (bool sucess, ) = payable(listings[_listingId].sellerAddress).call{
+            value: sendPrice
+        }("");
+        require(sucess, "Transfer Failed");
     }
 }
