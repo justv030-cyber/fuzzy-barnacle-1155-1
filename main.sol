153349c5251ffd4854b627d9d44213bcac8e9af5
justv030-cyber <justv030@gmail.com>
2026-09-11 16:24:53 +0530

    add contract withdraw func

--- main.sol
+++ main.sol
@@ -6,9 +6,14 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {ERC1155Pausable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
 
 contract Zuruki is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
     uint256 public publicMintPrice = 0.002 ether;
+
+    uint256 public maxSupply = 400;
+
+    mapping(address => uint256) public NFTMinted;
 
     constructor(
         address initialOwner
@@ -30,7 +35,11 @@ contract Zuruki is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
     }
 
     function mint(uint256 id, uint256 amount) public payable {
-        require(msg.value == publicMintPrice, "Not Enough Money For Mint!");
+        require(
+            msg.value == publicMintPrice * amount,
+            "Not Enough Money For Mint!"
+        );
+        require(totalSupply(id) + amount < maxSupply, "Sorry We Are Mint Out!");
         _mint(msg.sender, id, amount, "");
     }
 
@@ -52,5 +61,22 @@ contract Zuruki is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
         uint256[] memory values
     ) internal override(ERC1155, ERC1155Pausable, ERC1155Supply) {
         super._update(from, to, ids, values);
+    }
+
+    function uri(
+        uint256 _id
+    ) public view virtual override returns (string memory) {
+        require(exists(_id), "URI: This ID Does Not Exist");
+
+        return
+            string(
+                abi.encodePacked(super.uri(_id), Strings.toString(_id), ".json")
+            );
+    }
+
+    function withdraw()external onlyOwner{
+        uint256 getBalance  = address(this).balance;
+        (bool sucess , )= payable(msg.sender).call{value: getBalance}("");
+        require(sucess,"Transfer Failed");
     }
 }
