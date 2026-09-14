bde0f565937cace94ff4c2a5f5bb24188a12df63
justv030-cyber <justv030@gmail.com>
2026-09-15 00:24:32 +0530

    add supportsInterface fun

--- Receiver.sol
+++ Receiver.sol
@@ -1,16 +1,38 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.34;
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.34;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/IERC1155Receiver.sol";
+    import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/IERC1155Receiver.sol";
+    // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+    // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+    import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/introspection/ERC165.sol";
+    import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/introspection/IERC165.sol";
 
-abstract contract Receiver is IERC1155Receiver {
-    function onERC1155Received(
-        address operator,
-        address from,
-        uint256 id,
-        uint256 value,
-        bytes calldata data
-    ) external returns (bytes4) {
-        return  this.onERC1155Received.selector;
+    contract Receiver is IERC1155Receiver ,ERC165 {
+        function onERC1155Received(
+            address operator,
+            address from,
+            uint256 id,
+            uint256 value,
+            bytes calldata data
+        ) external returns (bytes4) {
+            return this.onERC1155Received.selector;
+        }
+
+        function onERC1155BatchReceived(
+            address operator,
+            address from,
+            uint256[] calldata ids,
+            uint256[] calldata values,
+            bytes calldata data
+        ) external returns (bytes4) {
+            return this.onERC1155BatchReceived.selector;
+        }
+
+        function supportsInterface(
+            bytes4 interfaceId
+        ) public view virtual override(ERC165, IERC165) returns (bool) {
+            return
+                interfaceId == type(IERC1155Receiver).interfaceId ||
+                super.supportsInterface(interfaceId);
+        }
     }
-}
